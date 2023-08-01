00bb6f646894d138dcebc2e2d57ab9a5e0b81b2b
Gabriel DM <gnizto@outlook.com>
2023-08-01 20:35:16 -0300

    Exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+  let result = "";
 
+  if (string === "") return string;
+  if (times < 0) return "ERROR";
+
+  for (let i = 0; i < times; i++) {
+    result += string;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
