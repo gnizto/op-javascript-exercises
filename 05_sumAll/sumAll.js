4eeaf0821dcdbce3a9abef32577b2da6eeda0632
Gabriel DM <gnizto@outlook.com>
2023-08-01 21:22:23 -0300

    Exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start != "number" ||
+    typeof end != "number"
+  )
+    return "ERROR";
 
+  // To change position when end is smaller than start
+  if (end < start) {
+    temp = start;
+    start = end;
+    end = temp;
+  }
+
+  sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
