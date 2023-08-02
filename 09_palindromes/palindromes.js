f80a4581141443e82c5fcde7f767e0c52fc4d6f5
Gabriel DM <gnizto@outlook.com>
2023-08-01 22:15:28 -0300

    Exercise 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  reversedString = cleanedString.split("").reverse().join("");
 
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
