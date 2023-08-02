7d93a05d7242d63385b9038a39e70d2eeda5bbd4
Gabriel DM <gnizto@outlook.com>
2023-08-02 20:24:51 -0300

    Execise 9 done - fix variable

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (string) {
-  cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
-  reversedString = cleanedString.split("").reverse().join("");
+  const cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversedString = cleanedString.split("").reverse().join("");
 
   return cleanedString === reversedString;
 };
