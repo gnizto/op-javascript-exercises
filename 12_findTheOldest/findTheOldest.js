2a0308fbb0ec066a3f1f9e4cd4dfb12232b85d6d
Gabriel DM <gnizto@outlook.com>
2023-08-02 23:02:16 -0300

    Exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPersonResult = people.reduce((oldestPerson, currentPerson) => {
+    let maxYearLived = currentPerson.yearOfDeath || new Date().getFullYear();
+    let currentPersonAge = maxYearLived - currentPerson.yearOfBirth;
 
+    maxYearLived = oldestPerson.yearOfDeath || new Date().getFullYear();
+    let oldestPersonAge = maxYearLived - oldestPerson.yearOfBirth;
+    return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+  });
+
+  return oldestPersonResult;
 };
 
 // Do not edit below this line
