367a5ee3ec6ad407266a23cc8f87f342a90dd99c
Gabriel DM <gnizto@outlook.com>
2023-08-02 22:25:44 -0300

    Exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+  const positionEvaluated = Number.parseInt(position);
+  if (!Number.parseInt(positionEvaluated)) return "OOPS";
+  if (positionEvaluated < 0) return "OOPS";
 
+  let previousA = 0;
+  let previousB = 1;
+  let currentPosition = 0;
+
+  for (let i = 1; i <= positionEvaluated; i++) {
+    currentPosition = previousA + previousB;
+    [previousB, previousA] = [previousA, currentPosition];
+  }
+  return currentPosition;
 };
 
 // Do not edit below this line
